293d914f43d57bfa243272e33cec01caaf7fb67d
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-09-23 13:14:34 -0600

    ActionCooldown and smarter way to handle animations player

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var animated_body = $Body
 @onready var animated_feet = $Feet
 @onready var shoot_cooldown = $ShootCooldown
-@onready var reload_cooldown = $ReloadCooldown
+@onready var action_cooldown: Timer = $ActionCooldown
 @onready var animation_player = $AnimationPlayer
 @onready var pistol_reload = $pistol_reload
 @onready var raycast = $RayCast2D
@@ -29,52 +29,18 @@ enum Hold {
 	KNIFE
 }
 
-enum ControllerType {
-	XBOX,
-	PLAYSTATION,
-	SWITCH,
-	SWITCH_JOYCON_LEFT,
-	SWITCH_JOYCON_RIGHT,
-	STEAM_DECK,
-	GENERIC
-}
-
-func _deduce_controller_type_from_name(controller_name: String) -> ControllerType:
-	if controller_name.contains("XInput") or\
-			controller_name.contains("Xbox"):
-		return ControllerType.XBOX
-	elif controller_name.contains("Sony") or\
-			controller_name.contains("Playstation") or\
-			controller_name.contains("PS4") or\
-			controller_name.contains("PS5") or \
-			controller_name.contains("DualSense"):
-		return ControllerType.PLAYSTATION
-	elif controller_name.contains("Joy-Con"):
-		if controller_name.contains("(L)"):
-			return ControllerType.SWITCH_JOYCON_LEFT
-		else:
-			return ControllerType.SWITCH_JOYCON_RIGHT
-	elif controller_name.contains("Switch"):
-		return ControllerType.SWITCH
-	elif controller_name.contains("Steam"): # SteamManager.is_on_steam_deck or
-		return ControllerType.STEAM_DECK
-	
-	return ControllerType.GENERIC
-
 class Player:
 	var state := State.IDLE
+	var last_state := State.IDLE
 	var hold := Hold.PISTOL
-
-	var can_shoot := true
-	var is_reloading := false
-	var is_punching := false
+	var last_hold := Hold.PISTOL
 
 	var bullets := MAX_BULLETS
 
 	var animated_body
 	var animated_feet
 	var shoot_cooldown
-	var reload_cooldown
+	var action_cooldown
 	var animation_player
 	var pistol_reload
 	var raycast
@@ -96,44 +62,30 @@ class Player:
 		Hold.KNIFE: "knife"
 	}
 
-	func _init(_animated_body, _animated_feet, _shoot_cooldown, _reload_cooldown, _animation_player, _pistol_reload, _raycast, _parent_node):
+	func _init(_animated_body, _animated_feet, _shoot_cooldown, _action_cooldown, _animation_player, _pistol_reload, _raycast, _parent_node):
 		self.animated_body = _animated_body
 		self.animated_feet = _animated_feet
 		self.shoot_cooldown = _shoot_cooldown
-		self.reload_cooldown = _reload_cooldown
+		self.action_cooldown = _action_cooldown
 		self.animation_player = _animation_player
 		self.pistol_reload = _pistol_reload
 		self.raycast = _raycast
 		self.parent_node = _parent_node
-		print("Player created")
-		print(animated_body) # <null>
-		print(animated_feet) # <null>
-		print(shoot_cooldown) # <null>
-		print(reload_cooldown) # <null>
-		print(animation_player) # <null>
-		print(pistol_reload) # <null>
-		print(raycast) # <null>
-		print(parent_node) # <CharacterBody2D#83399541991>
 
 	func update_animation():
-		# update_animation isn't called at the right time
-		# should be called after idle / move
-		# should be called after change weapon
-
-		# shoulbe be call change state / change hold
-
-		# print(hold_to_animation[hold] + "_" + state_to_animation[state])
-		animated_body.play(hold_to_animation[hold] + "_" + state_to_animation[state])
-
+		if state != last_state or hold != last_hold:
+			# animated_body.stop() # do we need this ?
+			animated_body.play(hold_to_animation[hold] + "_" + state_to_animation[state])
+			print(hold_to_animation[hold] + "_" + state_to_animation[state])
+			last_state = state
+			last_hold = hold
 
 	func shoot():
-		can_shoot = false
 		if bullets <= 0:
 			reload()
 			return
 		state = State.SHOOTING
 		shoot_cooldown.start()
-		update_animation()
 		animation_player.stop()
 		animation_player.play("shoot") # sound y flash
 		bullets -= 1
@@ -148,21 +100,16 @@ class Player:
 
 	func reload():
 		state = State.RELOAD
-		reload_cooldown.start()
-		is_reloading = true
+		action_cooldown.start()
 		pistol_reload.play()
 		bullets = MAX_BULLETS
-		update_animation()
 
 	func melee_attack():
 		state = State.PUNCH
-		reload_cooldown.start()
-		is_punching = true
-		update_animation()
+		action_cooldown.start()
 
 	func change_weapon():
-		reload_cooldown.start()
-		is_reloading = true
+		action_cooldown.start()
 		# for now
 		if hold == Hold.FLASHLIGHT:
 			hold = Hold.PISTOL
@@ -171,11 +118,10 @@ class Player:
 			hold = Hold.FLASHLIGHT
 			print("change to flashlight")
 
-var frame := 0 # for debug
 var player
 
 func _ready():
-	player = Player.new(animated_body, animated_feet, shoot_cooldown, reload_cooldown, animation_player, pistol_reload, raycast, self)
+	player = Player.new(animated_body, animated_feet, shoot_cooldown, action_cooldown, animation_player, pistol_reload, raycast, self)
 
 # do a boolean varaible "last_action_controller" to know if the last action was with the controller or mouse
 func rotation_player():
@@ -216,37 +162,31 @@ func _process(delta):
 		rotation = rotation_vector.angle()
 
 	var direction = direction_input
-	# direction.x *= 0.7 # Reduce diagonal speed
-	# direction = direction.rotated(rotation + (PI / 2))
 	if direction != Vector2.ZERO:
 		velocity = delta * direction * SPEED
 	else:
 		velocity = Vector2.ZERO
 		
-	frame += delta
-	if frame > 0.1:
-		frame = 0
-		#print(velocity.length())
 	
-	if Input.is_action_pressed('shoot') and player.can_shoot:
-		player.shoot()
-	
-	if Input.is_action_pressed('reload') and not player.is_reloading:
-		player.reload()
-	
-	if Input.is_action_pressed('melee_attack') and not player.is_punching:
-		player.melee_attack()
-	
-	if Input.is_action_pressed('change_weapon') and not player.is_reloading:
-		print("change weapon")
-		player.change_weapon()
 
-	if not animated_body.is_playing(): # if not state reload nor shoot
+	if action_cooldown.is_stopped():
+		if Input.is_action_pressed('shoot') and shoot_cooldown.is_stopped():
+			player.shoot()
+
+		if Input.is_action_pressed('reload'):
+			player.reload()
+		
+		if Input.is_action_pressed('melee_attack'):
+			player.melee_attack()
+		
+		if Input.is_action_pressed('change_weapon'):
+			player.change_weapon()
+
+	if action_cooldown.is_stopped() and shoot_cooldown.is_stopped():
 		if direction_input != Vector2.ZERO:
 			player.state = State.MOVE
 		else:
 			player.state = State.IDLE
-		player.update_animation()
 	
 	if direction_input != Vector2.ZERO:
 		if direction_input.x == 1:
@@ -257,15 +197,6 @@ func _process(delta):
 			animated_feet.play("walk")
 	else:
 		animated_feet.play("idle")
-	
+
+	player.update_animation()
 	move_and_slide()
-
-
-func _on_shoot_cooldown_timeout():
-	player.can_shoot = true
-
-
-func _on_reload_cooldown_timeout():
-	player.is_reloading = false
-	player.can_shoot = true
-	player.is_punching = false
